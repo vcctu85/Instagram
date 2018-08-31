da99c9197f8bd21a7f698591d3442c83d2477685
Vicky Tu <vcctu85@berkeley.edu>
2018-08-31 11:04:13 -0700

    successfully maneuvered to login screen when app opensgit add .

--- app/src/main/java/example/instagram2/Home/HomeActivity.java
+++ app/src/main/java/example/instagram2/Home/HomeActivity.java
@@ -1,6 +1,8 @@
 package example.instagram2.Home;
 
 import android.content.Context;
+import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
@@ -9,9 +11,12 @@ import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.ittianyu.bottomnavigationviewex.BottomNavigationViewEx;
 import com.nostra13.universalimageloader.core.ImageLoader;
 
+import example.instagram2.Login.LoginActivity;
 import example.instagram2.R;
 import example.instagram2.Utils.BottomNavigationViewHelper;
 import example.instagram2.Utils.SectionsPagerAdapter;
@@ -22,17 +27,26 @@ public class HomeActivity extends AppCompatActivity {
     private Context mContext = HomeActivity.this;
     private static final int ACTIVITY_NUM = 0;
 
+    //firebase
+    private FirebaseAuth  mAuth;
+    private FirebaseAuth.AuthStateListener mAuthListener;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
         Log.d(TAG, "onCreate: starting.");
 
+        setupFirebaseAuth();
         initImageLoader();
         setupBottomNavigationView();
         setupViewPager();
 
     }
+
+
+
     /** BottomNavigationView setup */
     private void setupBottomNavigationView() {
         Log.d(TAG, "setupBottomNavigationView: setting up");
@@ -62,5 +76,57 @@ public class HomeActivity extends AppCompatActivity {
         tabLayout.getTabAt(0).setIcon(R.drawable.ic_camera);
         tabLayout.getTabAt(1).setIcon(R.drawable.ic_icon);
         tabLayout.getTabAt(2).setIcon(R.drawable.ic_arrow);
+    }
+    //---------------------Firebase---------------------
+
+    /**
+     * checking if user is logged in
+     * @param user
+     */
+    private void checkCurrentUser(FirebaseUser user) {
+        Log.d(TAG, "checkCurrentUser: checking if user is logged in");
+
+        if (user == null) {
+            Intent intent = new Intent(mContext, LoginActivity.class);
+            startActivity(intent);
+        }
+    }
+    /**
+     * Setup the firebase auth object
+     * listener will passively check
+     */
+    private void setupFirebaseAuth() {
+        Log.d(TAG, "setupFirebaseAuth: setting up firebase auth");
+        mAuth = FirebaseAuth.getInstance();
+        mAuthListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                //check if user is logged in
+                checkCurrentUser(user);
+                if (user != null) {
+                    //user is already signed up
+                    Log.d(TAG, "onAuthStateChanged: signed_in:" + user.getUid());
+
+                } else {
+                    Log.d(TAG, "onAuthStateChanged: signed_out");
+                }
+            }
+        };
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        mAuth.addAuthStateListener(mAuthListener);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        if (mAuthListener != null) {
+            mAuth.removeAuthStateListener(mAuthListener);
+        }
     }
 }
